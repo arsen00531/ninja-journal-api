1ae746ef1f35668b7056b63555d02210b5e331d1
Арсен <arsendag505@gmail.com>
2024-02-10 16:59:29 +0300

    delete .env

--- apps/sockets/src/app.module.ts
+++ apps/sockets/src/app.module.ts
@@ -2,7 +2,7 @@ import { CommonConfigModule } from "@app/common";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SocketModule } from './socket/socket.module';
-import { SocketService } from "./socket/socket.controller";
+import { SocketService } from "./socket/socket.service";
 
 @Module({
     imports: [
